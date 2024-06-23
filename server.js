3e7c78698275f0f99141626b461015605d34fe85
zacharymor <morzachary@gmail.com>
2024-06-22 20:01:54 -0400

    working v0

--- server.js
+++ server.js
@@ -3,104 +3,6 @@ const http = require('http');
 const socketIo = require('socket.io');
 const cors = require('cors');
 
-class Broad {
-    constructor(id) {
-        this.c = 0;
-        this.id = id;
-    }
-    connection() {
-        this.c += 1;
-    }
-    disconnection() {
-        this.c -= 1;
-    }
-}
-
-class Heap {
-
-    constructor() {
-        this.heap = [];
-        this.wsidx = {};
-    }
-
-    getParentIndex(index) {
-        return Math.floor((index - 1) / 4);
-    }
-
-    getChildIndices(index) {
-        return [
-            4 * index + 1,
-            4 * index + 2,
-            4 * index + 3,
-            4 * index + 4
-        ];
-    }
-
-    swap(index1, index2) {
-        [this.heap[index1], this.heap[index2]] = [this.heap[index2], this.heap[index1]];
-        [this.wsidx[this.heap[index1].id], this.wsidx[this.heap[index2].id]] = [this.wsidx[this.heap[index2].id], this.wsidx[this.heap[index1].id]];
-    }
-
-    insert(value) {
-        this.heap.push(value);
-        this.wsidx[value.id] = this.heap.length - 1;
-        this.heapifyUp();
-        this.logDesc()
-    }
-
-    heapifyUp() {
-        let index = this.heap.length - 1;
-        while (index > 0) {
-            let parentIndex = this.getParentIndex(index);
-            if (this.heap[parentIndex].c > this.heap[index].c) {
-                this.swap(parentIndex, index);
-                index = parentIndex;
-            } else {
-                break;
-            }
-        }
-    }
-
-    extractMin() {
-        if (this.heap.length === 0) {
-            return null;
-        }
-        if (this.heap.length === 1) {
-            return this.heap.pop();
-        }
-        const min = this.heap[0];
-        this.heap[0] = this.heap.pop();
-        this.heapifyDown(0);
-        return min;
-    }
-
-    logDesc() {
-        console.log(this.heap.length + " broads");
-        this.heap.forEach((e,i) => {
-            console.log('level: ' + Math.floor(i/4));
-            console.log('broad c: ' + e.c);
-            console.log('broad id: ' + e.id);
-        });
-        console.log(this.wsidx);
-    }
-
-    heapifyDown(index) {
-        let smallest = index;
-        const childrenIndices = this.getChildIndices(index);
-
-        for (let i of childrenIndices) {
-            if (i < this.heap.length && this.heap[i].c < this.heap[smallest].c) {
-                smallest = i;
-            }
-        }
-
-        if (smallest !== index) {
-            this.swap(index, smallest);
-            this.heapifyDown(smallest);
-        }
-    }
-}
-
 
 const app = express();
 const server = http.createServer(app);
@@ -114,12 +16,15 @@ const io = socketIo(server, {
 });
 
 let broadcaster;
+let broadcastery;
 let viewers = [];
 let voters = [];
-let netwrok = new Heap();
 
 const randomViewer = (xcid) => {
-    return viewers.filter(e => e != xcid)[Math.floor(Math.random() * (viewers.length - 1))];
+    let rando = viewers.filter(e => e != xcid)[Math.floor(Math.random() * (viewers.length - 1))];
+    broadcastery = rando;
+    console.log(rando + ' was randomly selected to be next')
+    return rando;
 };
 
 app.use(express.static(__dirname + '/public'));
@@ -129,41 +34,47 @@ app.use(cors({
 
 io.on('connection', socket => {
     console.log(`${socket.id} connected`);
-
-    socket.on('broadcaster', () => {
-        broadcaster = socket.id;
-        netwrok = new Heap(socket.id);
-        netwrok.insert(new Broad(socket.id));
-        socket.broadcast.emit('broadcaster');
-    });
-
-    socket.on('peered', () => {
-        let broadId = netwrok.wsidx[socket.id];
-        netwrok.heap[broadId].connection();
-        netwrok.logDesc();
-    });
+    if (!broadcaster) {
+        console.log(`offering broadcast`);
+        socket.emit('selected');
+    }
 
     socket.on('vote', () => {
+
         if (!voters.includes(socket.id)) {
             voters.push(socket.id);
+            console.log('there are now ' + voters.length + ' votes');
+            voters.forEach(e => {
+                console.log(e)
+            });
         }
-        if (voters.length > (viewers.length/2)) {
-            console.log(voters.length)
-            console.log('Rotate');
+
+
+
+        if (voters.length > (viewers.length / 5)) {
+            io.to(randomViewer()).emit('selected');
+        }
+    });
+
+
+    socket.on('broadcaster', () => {
+        if (broadcaster) {
+            console.log(socket.id + ' has taken the stick')
+            viewers = [];
             voters = [];
-            console.log('voters reset to ' + voters.length)
         }
-        console.log(voters.length + ' votes')
+        broadcaster = socket.id;
+        socket.broadcast.emit('broadcaster', socket.id);
+    });
+
+    socket.on('selected', () => {
+        console.log(socket.id + " has taken the stick");
     });
 
     socket.on('watcher', () => {
         viewers.push(socket.id);
         socket.emit('role', 'viewer');
         socket.to(broadcaster).emit('watcher', socket.id);
-        if ((netwrok.heap.length > 0 )&&(netwrok.heap[netwrok.heap.length - 1].c > 2)) {
-            console.log('[!] branch time');
-            // turn watchers into broads
-        }
     });
 
     socket.on('offer', (id, message) => {
@@ -174,20 +85,24 @@ io.on('connection', socket => {
         socket.to(id).emit('answer', socket.id, message);
     });
 
+    socket.on('candidate', (id, message) => {
+        socket.to(id).emit('candidate', socket.id, message);
+    });
+
     socket.on("rotate", () => {
         console.log("rototototo");
         socket.to(randomViewer(socket.id)).emit('selected');
     });
 
-    socket.on('candidate', (id, message) => {
-        socket.to(id).emit('candidate', socket.id, message);
-    });
-
     socket.on('disconnect', () => {
-        
+
         if (socket.id === broadcaster) {
             broadcaster = null;
+            io.emit('selected');
             io.emit('broadcaster');
+        } else if (socket.id === broadcastery) {
+            broadcastery = null;
+            socket.to(randomViewer(socket.id)).emit('selected');
         } else {
             viewers = viewers.filter(viewer => viewer !== socket.id);
             voters = voters.filter(voter => voter !== socket.id);
